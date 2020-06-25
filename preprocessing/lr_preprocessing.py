b6f231a41c8b34257f90eab4943fa199fe1f92a6
LotanLevy <lotan.levy@mail.huji.ac.il>
2020-06-25 12:28:45 +0300

    add naomi dataset

--- preprocessing/lr_preprocessing.py
+++ preprocessing/lr_preprocessing.py
@@ -13,6 +13,7 @@ import numpy as np
 def lr_preprocessing(images_dir, output_dir, size):
     images_paths = os.listdir(images_dir)
     for name in images_paths:
+        print(os.path.join(images_dir, name))
         image = Image.open(os.path.join(images_dir, name))
         lr_image = image.resize(size=size)
         lr_image.save(os.path.join(output_dir, name))
@@ -32,6 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--lr-img-size', type=int, nargs=2, default=(128, 128))
     args = parser.parse_args()
 
-    os.makedirs(args.lr_imgs_dir)
+    if not os.path.exists(args.lr_imgs_dir):
+        os.makedirs(args.lr_imgs_dir)
 
     lr_preprocessing(args.img_dir, args.lr_imgs_dir, args.lr_img_size)
